787a77b657c3f2711afcf2d33a9d82dc94c4e34a
theokoles7 <61356329+theokoles7@users.noreply.github.com>
2021-12-14 13:01:16 -0600

    Add files via upload

---
+++ Java/Implementation/CountApplesAndOranges.java
@@ -0,0 +1,21 @@
+package Implementation;
+import java.util.*;
+
+public class CountApplesAndOranges {
+  public static void main(String args[]){
+    countApplesAndOranges(7, 11, 5, 15, Arrays.asList(-2, 2, 1), Arrays.asList(5, -6));
+    System.out.println();
+    countApplesAndOranges(2, 3, 1, 5, Arrays.asList(2), Arrays.asList(-2));
+  }  
+
+  public static void countApplesAndOranges(int l, int r, int a, int o, List<Integer> apples, List<Integer> oranges){
+    int count_a = 0, count_o = 0;
+    for(int i = 0; i < apples.size(); i++){
+      if(a + apples.get(i) >= l && a + apples.get(i) <= r){count_a++;}
+    }
+    for(int i = 0; i < oranges.size(); i++){
+      if(o + oranges.get(i) >= l && o + oranges.get(i) <= r){count_o++;}
+    }
+    System.out.print(count_a + "\n" + count_o);
+  }
+}
---
+++ Java/Implementation/GradingStudents.java
@@ -0,0 +1,18 @@
+package Implementation;
+import java.util.*;
+
+public class GradingStudents {
+  public static void main(String args[]){
+    List<Integer> grades = Arrays.asList(73, 67, 38, 33);
+    System.out.println(grades + " => " + gradingStudents(grades));
+  }
+
+  public static List<Integer> gradingStudents(List<Integer> grades){
+    for(int i = 0; i < grades.size(); i++){
+      if(grades.get(i) >= 38 && (5 - grades.get(i) % 5 == 1 || 5 - grades.get(i) % 5 == 2)){
+        grades.set(i, grades.get(i) + (5 - (grades.get(i) % 5)));
+      }
+    }
+    return grades;
+  }
+}

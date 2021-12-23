55b6c8519f05d22205697d71b6ecff58c4b4d705
theokoles7 <61356329+theokoles7@users.noreply.github.com>
2021-12-23 12:35:30 -0600

    Add files via upload

---
+++ Java/Implementation/HurdleRace.java
@@ -0,0 +1,20 @@
+package Implementation;
+
+import java.util.*;
+
+public class HurdleRace {
+  public static void main(String args[]){
+    System.out.println(hurdleRace(4, Arrays.asList(1, 6, 3, 5, 2)));
+    System.out.println(hurdleRace(7, Arrays.asList(2, 5, 4, 5, 2)));
+  }
+
+  public static int hurdleRace(int k, List<Integer> height){
+    int max = 0;
+    for(int h : height){
+      if(h > k){
+        max = Math.max(max, h - k);
+      }
+    }
+    return max;
+  }
+}

f5a8e39edb676349eefa3d0eb6b5374944afa6cb
theokoles7 <61356329+theokoles7@users.noreply.github.com>
2021-12-27 09:22:17 -0600

    Add files via upload

---
+++ Java/Implementation/BeautifulDays.java
@@ -0,0 +1,25 @@
+package Implementation;
+
+public class BeautifulDays {
+  public static void main(String args[]){
+    System.out.println(beautifulDays(20, 23, 6));
+    System.out.println(reverse(5467));
+  }
+
+  public static int beautifulDays(int i, int j, int k){
+    int count = 0;
+    for(int t = i; t <= j; t++){
+      if((t - reverse(t)) % k == 0){count++;}
+    }
+    return count;
+  }
+
+  public static int reverse(int n){
+    int rev = 0;
+    while(n != 0){
+      rev = (rev * 10) + (n % 10);
+      n = n / 10;
+    }
+    return rev;
+  }
+}
